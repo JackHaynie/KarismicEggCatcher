4fcde1b0e821226a14b40ee3d218ecf7f7d1b0b3
Jack Haynie <me@jackhaynie.com>
2019-02-14 00:48:47 -0500

    Add files via upload

---
+++ KarismicEggCatcher/src/me/JackHaynie/Karismic/listeners/EggCatcherPlayerListener.java
@@ -0,0 +1,8 @@
+package me.JackHaynie.Karismic.listeners;
+
+import org.bukkit.event.Listener;
+
+public class EggCatcherPlayerListener implements Listener {
+
+
+}
---
+++ KarismicEggCatcher/src/me/JackHaynie/Karismic/listeners/EggCommand.java
@@ -0,0 +1,4 @@
+package me.JackHaynie.Karismic.listeners;
+
+public class EggCommand {
+}
